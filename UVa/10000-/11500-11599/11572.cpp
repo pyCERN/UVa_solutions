f62c62ee495c1e7d654232672eecb492ab82ec90
cd ab <gjk6626@gmail.com>
2021-09-01 04:13:02 +0900

    Update 11572

---
+++ UVa/10000-/11500-11599/11572.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+map<int, int> m;
+
+int main(void){
+    int TC;
+    int n_snow;
+    scanf("%d", &TC);
+
+    while(TC--){
+        int snow, length = 0, max_length = 0;
+        scanf("%d", &n_snow);
+
+        for(int i = 1; i <= n_snow; i++){
+            scanf("%d", &snow);
+            int idx = m[snow];
+            if(idx != 0){ // Found
+                if(i - idx > length) length++;
+                else length = i - idx;
+            }
+            else length++;
+            m[snow] = i;
+            if(max_length < length) max_length = length;
+        }
+        m.clear();
+        printf("%d\n", max_length);
+    }
+
+    return 0;
+}
