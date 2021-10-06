11b57ae62c4b0211810bbbd6761f8e9f5488396c
pycern <39230856+pyCERN@users.noreply.github.com>
2021-10-07 02:09:04 +0900

    Add TSP.cpp

--- TSP.cpp
+++ TSP.cpp
@@ -2,34 +2,48 @@
 #include <bitset>
 using namespace std;
 
+#define MIN 100000001
 // DP
 
 /*
-tsp[pos][visit]
-total weight of visit state at pos
+W[i][j]
+weight for i -> j
 */
 int N;
-int adj_graph[16][16];     // weight matrix between cities
-int tsp[16][65536] = {0,};
-bitset<16> visited;
+int W[16][16];
+int weight_dp[16][65536];
 
-void tour(void){
-    tsp[0][1] = adj_graph[0][0]; // start at pos 0
-    for(int cur_pos = 0; cur_pos < N; cur_pos++){
-        tsp[cur_pos][(1<<N)-1] = adj_graph[cur_pos][0];
-        for(int nxt_pos = 0; nxt_pos < N; nxt_pos++){
-            int mask = visited.to_ulong();
-            if((nxt_pos != cur_pos) && (mask & (1 << nxt_pos) == 0)){ // not cur_pos and not visited
-                tsp[cur_pos][mask] = 
-            }
+int tsp(int cur_pos, int visit_state){ // return total weight for visit_state at cur_pos
+    if(visit_state == (1<<N)-1 && W[cur_pos][0] != 0) // visit all cities if the last city is connected to the initial city
+        return W[cur_pos][0];
 
+    if(weight_dp[cur_pos][visit_state] != -1) // if already computed
+        return weight_dp[cur_pos][visit_state];
+
+    int min_weight = MIN;
+    for(int nxt_pos = 0; nxt_pos < N; nxt_pos++){
+        if(!(visit_state & (1<<nxt_pos)) && W[cur_pos][nxt_pos]){ // nxt_pos not visited
+            int weight = W[cur_pos][nxt_pos] + tsp(nxt_pos, visit_state | (1<<nxt_pos)); // move on to nxt_pos
+            min_weight = min(min_weight, weight);
         }
+    }
+    weight_dp[cur_pos][visit_state] = min_weight;
+    return min_weight;
 }
 
 int main(void){
     scanf("%d", &N);
     for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++) scanf("%d", adj_graph[i][j]);
+        for(int j = 0; j < N; j++) scanf("%d", &W[i][j]);
     }
 
+    for(int pos = 0; pos < N; pos++){
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < (1<<N); j++) weight_dp[i][j] = -1;
+        }
+    }
+    tsp(0, 1);
+    printf("%d\n", weight_dp[0][1]);
+
+    return 0;
 }
