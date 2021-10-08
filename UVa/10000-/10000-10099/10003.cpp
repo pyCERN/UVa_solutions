150bf5681f44bca33bcd54295d1dbbde54bd639f
cd ab <gjk6626@gmail.com>
2021-10-08 13:44:33 +0900

    Update 10003.cpp

---
+++ UVa/10000-/10000-10099/10003.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+using namespace std;
+
+/*
+
+*/
+int L, n;
+int A[52];
+
+int cut(int left, int right){
+
+}
+
+int main(void){
+    scanf("%d %d", &L, &n);
+    A[0] = 0; A[n+1] = L;
+    for(int i = 1; i < n+1; i++) scanf("%d", &A[i]);
+
+}
