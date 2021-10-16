9fe9c4b765af70c29690a05752cdf31215272ab9
cd ab <gjk6626@gmail.com>
2021-10-16 18:23:26 +0900

    Add 1103.cpp

--- UVa/100-1999/1100-1199/1103.cpp
+++ UVa/100-1999/1100-1199/1103.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
-#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 // Find connected component
@@ -7,117 +9,103 @@ using namespace std;
 Num of holes
 A J D S W K
 1 3 5 4 0 2
-
-A
-........
-.11111..
-.1...1..
-.11111..
-..11....
-........
-
-DWWW
-........
-.111111.
-.1....1.
-.111111.
-.1....1.
-.111111.
-.1....1.
-.111111.
-.1....1.
-.111111.
-.1....1.
-.111111.
-........
-........
-.1..1.1.
-........
-
-AKW
-............
-.111.1111...
-.1.1.1..1...
-.111.1..1...
-.1.1.1111...
-.111........
-............
-.1111111....
-...1111.....
-............
-
-AWW
-....
-...1
-..11
-..1.
-1...
-111.
-1111
-.1.1
-.111
-.11.
-.1..
-11..
-11.1
-1..1
-1.11
-1.1.
-
-........
-.111111.
-.1....1.
-.111111.
-.1....1.
-.111111.
-.1....1.
-.111111.
-........
 */
 
 int H, W;
-char hieroglyphs[200][200];
-// int hieroglyphs_copy[200][50];
-// E, W, S, N, EN, WN, WS, ES
-int dx[] = {1, -1, 0, 0, 1, -1, -1, 1};
-int dy[] = {0, 0, 1, -1, -1, -1, 1, 1};
+int hieroglyphs[210][210], letter[210][210];
+vector<char> ans;
+char ans_letter[] = "WAKJSD";
+// E, W, S, N
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+int hex_to_bin[][4] = {
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 1},
+                        {0, 0, 1, 0},
+                        {0, 0, 1, 1},
+                        {0, 1, 0, 0},
+                        {0, 1, 0, 1},
+                        {0, 1, 1, 0},
+                        {0, 1, 1, 1},
+                        {1, 0, 0, 0},
+                        {1, 0, 0, 1},
+                        {1, 0, 1, 0},
+                        {1, 0, 1, 1},
+                        {1, 1, 0, 0},
+                        {1, 1, 0, 1},
+                        {1, 1, 1, 0},
+                        {1, 1, 1, 1}
+                       };
 
-int floodfill(int x, int y, char c1, char c2, char c3, bool is_letter){ // '1', '0', '.'
-    int ans = 1;
+void floodfill(int grid[210][210], int x, int y, int c1, int c2, int c3){ // 1, 2, (1, 2)
+    int num = 1;
 
-    if(x < 0 | x >= W | y < 0 | y >= H) return 0;
-    if(hieroglyphs[x][y] != c1) return 0;
-    // if(hieroglyphs[x][y] == c2{
-    //     if(is_letter){
+    if(x < 0 | x > 4*W+1 | y < 0 | y > H+1) return;
+    if(grid[y][x] != c1) return;
 
-    //     }
-    //     else return;
-    // }
-
-    // Pass conditions -> letter exists
-    hieroglyphs[x][y] = c3;
+    if(grid != letter)
+        hieroglyphs[y][x] = c2;
+    letter[y][x] = c3;
 
     for(int i = 0; i < 8; i++){
-        ans += floodfill(x+dx[i], y+dy[i], c1, c2, c3, true);
+        floodfill(grid, x+dx[i], y+dy[i], c1, c2, c3);
+    }
+}
+
+int get_letter(void){
+    int num, n_holes = 0;
+
+    for(int y = 0; y <= H+1; y++){
+        for(int x = 0; x <= 4*W; x++){
+            if(letter[y][x] == 1) letter[y][x] = 0;
+            else letter[y][x] = 1;
+        }
+    }
+
+    for(int y = 0; y <= H+1; y++){
+        for(int x = 0; x <= 4*W+1; x++){
+            if(letter[y][x] == 1){
+                floodfill(letter, x, y, 1, 2, 2);
+                n_holes++;
+            }
+        }
     }
     
-    // Letter found
-    return ans;
+    return ans_letter[n_holes-1];
 }
 
 int main(void){
     int TC = 0;
 
     while(scanf("%d %d", &H, &W), H | W){
+        if(TC) printf("\n");
         char str[200];
         memset(hieroglyphs, 0, sizeof(hieroglyphs));
+        ans.clear();
 
         for(int i = 0; i < H; i++){
-            fgets(str, W, stdin);
+            scanf("%s", str);
             for(int j = 0; j < W; j++){
-                str[i]
+                int hex = 0;
+                if(str[j] >= 97) hex = str[j] - 97 + 10;
+                else hex = str[j] - 48;
+                for(int k = 0; k < 4; k++){
+                    hieroglyphs[i+1][4*j+k+1] = hex_to_bin[hex][k];
+                }
             }
         }
+        for(int y = 0; y <= H+1; y++){
+            for(int x = 0; x <= 4*W+1; x++){
+                if(hieroglyphs[y][x] == 1){ // floodfill one letter
+                    memset(letter, 0, sizeof(letter));
+                    floodfill(hieroglyphs, x, y, 1, 2, 1);
+                    ans.push_back(get_letter());
+                }
+            }
+        }
+        sort(ans.begin(), ans.end());
+        printf("Case %d: ", ++TC);
+        for(int i = 0; i < ans.size(); i++) printf("%c", ans[i]);
     }
 
     return 0;
