91296f2705b97da1be79cdbdff64959464234d07
cd ab <gjk6626@gmail.com>
2021-10-17 20:14:33 +0900

    Update 11060.py

--- UVa/10000-/11000-11099/11060.py
+++ UVa/10000-/11000-11099/11060.py
@@ -1,53 +1,19 @@
 from collections import deque
 
 
-adj_list = {}
-adj_rev_list = {}
-drinked = {}
-queue = deque()
-'''
-0: []
-1: [0]
-2: [0, 1]
-3: [1, 2]
-4: [3]
-5: [2]
-6: [7]
-7: []
-
-0: [1, 2]
-1: [2, 3]
-2: [3, 5]
-3: [4]
-4: []
-5: []
-6: []
-7: [6]
-
-[1, 2
-'''
-def bfs(bev):
-    drink_queue = deque()
-    drink_queue.append(bev)
-
-    while drink_queue:
-        
-    
+in_edge = {}
+out_edge = {}
+queue = deque() # set of nodes with no in-edge
 
 N = int(input())
-for _ in range(N):
+for i in range(N):
     bev = input()
-    adj_list[bev] = []
-    drinked[bev] = False
-    
+    in_edge[bev] = []
+    out_edge[bev] = []
 
 M = int(input())
 for _ in range(M):
     bev1, bev2 = input().split(' ')
-    adj_list[bev2].append(bev1)
-    adj_rev_list[bev1].append(bev2)
+    in_edge[bev2].append(bev1)
+    out_edge[bev1].append(bev2)
 
-for bev in adj_rev_list.keys():
-    if adj_rev_list[bev] == []:
-        queue.append(bev)
-        bfs(bev)
