d10e7ec6bce4499d220285ffd0766c97874ac401
cd ab <gjk6626@gmail.com>
2021-10-20 00:49:47 +0900

    Update UnionFind

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -5,26 +5,29 @@ typedef vector<int, int> vi;
 
 class UnionFind{
 private:
-    vi p, rank;
+    vi parent, rank;
 public:
     UnionFind(int N){
-        p.assign(N, 0);
-        rank.assign(N, 0);
-        for(int i = 0; i < N; i++) p[i] = i;
+        parent.assign(N, 0); rank.assign(N, 0);
+        for(int i = 0; i < N; i++) parent[i] = i;
     }
-    int findSet(int i){
-        return p[i];
+    int findSet(int u){
+        if(parent[u] == u) return u;
+        else{
+            parent[u] = findSet(parent[u]);
+            return parent[u];
+        }
     }
-    bool isSameSet(int i, int j){
-        return p[i] == p[j];
+    bool isSameSet(int u, int v){
+        return (findSet(u) == findSet(v));
     }
-    void unionSet(int i, int j){
-        int root_i = findSet(i), root_j = findSet(j);
-        if(!isSameSet(i, j)){
-            if(rank[i] < rank[j]) p[i] = root_j;
+    void unionSet(int u, int v){
+        int x = findSet(u), y = findSet(v);
+        if(x != y){
+            if(rank[x] > rank[y]) parent[y] = x;
             else{
-                p[j] = root_i;
-                if(rank[i] == rank[j]) rank[i]++;
+                parent[x] = y;
+                if(rank[x] == rank[y]) rank[y]++;
             }
         }
     }
