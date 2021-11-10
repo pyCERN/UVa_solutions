f3ef628e71b2efa9eb1b98bfd20857c76d0f24a1
cd ab <gjk6626@gmail.com>
2021-11-11 04:49:46 +0900

    Update 10684.cpp

--- UVa/10000-/10600-10699/10684.cpp
+++ UVa/10000-/10600-10699/10684.cpp
@@ -3,6 +3,10 @@
 #include <cstring>
 using namespace std;
 
+int max(int a, int b){
+    return a > b ? a : b;
+}
+
 int main(void){
     int N;
     int seq[10010];
@@ -12,11 +16,9 @@ int main(void){
         memset(seq, 0, sizeof(seq));
         for(int i = 0; i < N; i++) scanf("%d", &seq[i]);
         for(int i = 0; i < N; i++){
-            sum = 0;
-            for(int j = i; j < N; j++){
-                sum += seq[j];
-                if(ans < sum) ans = sum;
-            }
+            sum += seq[i];
+            ans = max(ans, sum);
+            sum = max(sum, 0);
         }
         if(ans > 0) printf("The maximum winning streak is %d.\n", ans);
         else printf("Losing streak.\n");
