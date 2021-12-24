5cfdc11de52f3a140f4338bf5e483b0d8a8cc3a1
pyCERN <gjk6626@gmail.com>
2021-12-24 23:09:43 +0900

    Update 10131.cpp

--- UVa/10000-/10100-10199/10131.cpp
+++ UVa/10000-/10100-10199/10131.cpp
@@ -1,67 +1,54 @@
 #include <cstdio>
+#include <cstring>
 #include <vector>
-#include <stack>
 #include <algorithm>
-#define MIN -10001
 using namespace std;
 
 typedef pair<pair<int, int>, int> iii;
 
-// (W, IQ, idx)
 vector<iii> ele;
-vector<int> sortedEle, eleIdx;
-stack<int> ans;
+int LIS_dp[1010];
+int N;
 
 bool compare(const iii &a, const iii &b){
-    if(a.first.first < b.first.first) return true;
-    else if(a.first.first == b.first.first) return a.first.second < b.first.second;
-    return false;
+    return a.first.first < b.first.first;
+}
+
+int LIS(int i){
+    if(LIS_dp[i] != -1) return LIS_dp[i];
+    LIS_dp[i] = 1;
+    for(int j = i+1; j < N; j++){
+        // if weight/IQ in increasing/decreasing order
+        if(ele[i].first.first < ele[j].first.first && ele[i].first.second > ele[j].first.second){
+            LIS_dp[i] = max(LIS_dp[i], LIS(j)+1);
+        }
+    }
+
+    return LIS_dp[i];
 }
 
 int main(void){
     int w, iq, idx = 1;
-    int LIS = 0;
-    FILE *fp = fopen("ans.txt", "w");
+    int maxLIS = 0;
 
     while(scanf("%d %d", &w, &iq) == 2){
-        ele.push_back(make_pair(make_pair(w, -iq), idx++));
+        ele.push_back(make_pair(make_pair(w, iq), idx++));
     }
-    // sort by weight
-    sort(ele.begin(), ele.end(), compare);
+    
+    N = ele.size();
+    sort(ele.begin(), ele.end());
+    memset(LIS_dp, -1, sizeof(LIS_dp));
 
-    for(auto i : ele) fprintf(fp, "%d %d %d\n", i.first.first, i.first.second, i.second);
-    fprintf(fp, "\n");
+    for(int i = 0; i < N; i++) maxLIS = max(maxLIS, LIS(i));
+    printf("%d\n", maxLIS);
 
-    // LDS by IQ
-    sortedEle.push_back(MIN);
-    for(int i = 0; i < idx-1; i++){
-        if(i > 0 && ele[i-1].first.first == ele[i].first.first){
-            eleIdx.push_back(-1);
-            continue;
-        }
-        if(ele[i].first.second > sortedEle[LIS]){
-            sortedEle.push_back(ele[i].first.second);
-            eleIdx.push_back(++LIS);
-        }
-        else{
-            auto it = lower_bound(sortedEle.begin(), sortedEle.end(), ele[i].first.second);
-            *it = ele[i].first.second;
-            eleIdx.push_back(it-sortedEle.begin());
+    for(int i = 0; i < N; i++){
+        if(LIS_dp[i] == maxLIS){
+            printf("%d\n", ele[i].second);
+            maxLIS--;
+            if(maxLIS == 0) break;
         }
     }
-    for(auto i : eleIdx) fprintf(fp, "%d\n", i);
 
-    printf("\n%d\n", LIS);
-    for(int i = eleIdx.size(); i >= 0; i--){
-        if(eleIdx[i] == LIS){
-            ans.push(i);
-            LIS--;
-        }
-    }
-    while(!ans.empty()){
-        fprintf(fp, "%d %d %d\n", ele[ans.top()].first.first, ele[ans.top()].first.second, ele[ans.top()].second);
-        ans.pop();
-    }
-    fclose(fp);
     return 0;
 }
