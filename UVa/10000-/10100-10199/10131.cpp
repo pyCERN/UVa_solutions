d078d252930380d2a28277e30c8934d16761b4a9
pycern <39230856+pyCERN@users.noreply.github.com>
2021-12-26 23:34:21 +0900

    Update 10131.cpp

--- UVa/10000-/10100-10199/10131.cpp
+++ UVa/10000-/10100-10199/10131.cpp
@@ -1,55 +1,58 @@
 // UVa 10131 - Is Bigger Smarter?
 // Sort + Longest Decreasing Sequence (LDS)
 #include <cstdio>
-#include <cstring>
 #include <vector>
+#include <stack>
 #include <algorithm>
 using namespace std;
 
 typedef pair<pair<int, int>, int> iii;
 
-vector<iii> ele;
+vector<iii> elephants;
 int LIS_dp[1010];
-int N;
+int N, nLIS;
 
 bool compare(const iii &a, const iii &b){
     return a.first.first < b.first.first;
 }
 
-int LIS(int i){
-    if(LIS_dp[i] != -1) return LIS_dp[i];
-    LIS_dp[i] = 1;
-    for(int j = i+1; j < N; j++){
-        // if weight/IQ in increasing/decreasing order
-        if(ele[i].first.first < ele[j].first.first && ele[i].first.second > ele[j].first.second){
-            LIS_dp[i] = max(LIS_dp[i], LIS(j)+1);
+void LIS(void){
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            // if weight/IQ in increasing/decreasing order
+            if(elephants[i].first.first < elephants[j].first.first && elephants[i].first.second > elephants[j].first.second) LIS_dp[j] = max(LIS_dp[j], LIS_dp[i]+1);
         }
+        nLIS = max(nLIS, LIS_dp[i]);
     }
-
-    return LIS_dp[i];
 }
 
 int main(void){
     int w, iq, idx = 1;
-    int maxLIS = 0;
+    stack<int> ans;
 
     while(scanf("%d %d", &w, &iq) == 2){
-        ele.push_back(make_pair(make_pair(w, iq), idx++));
+        elephants.push_back(make_pair(make_pair(w, iq), idx++));
     }
     
-    N = ele.size();
-    sort(ele.begin(), ele.end());
-    memset(LIS_dp, -1, sizeof(LIS_dp));
+    N = elephants.size(); nLIS = 0;
+    sort(elephants.begin(), elephants.end());
 
-    for(int i = 0; i < N; i++) maxLIS = max(maxLIS, LIS(i));
-    printf("%d\n", maxLIS);
+    for(int i = 0; i < N; i++) LIS_dp[i] = 1;
 
-    for(int i = 0; i < N; i++){
-        if(LIS_dp[i] == maxLIS){
-            printf("%d\n", ele[i].second);
-            maxLIS--;
-            if(maxLIS == 0) break;
+    LIS();
+    printf("%d\n", nLIS);
+
+    for(int i = N-1; i >= 0; i--){
+        if(LIS_dp[i] == nLIS){
+            ans.push(elephants[i].second);
+            nLIS--;
+            if(nLIS == 0) break;
         }
+    }
+    
+    while(!ans.empty()){
+        printf("%d\n", ans.top());
+        ans.pop();
     }
 
     return 0;
