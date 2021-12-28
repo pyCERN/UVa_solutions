3b9cda6014396242898f975c0152c50d1d765953
pyCERN <gjk6626@gmail.com>
2021-12-28 20:55:26 +0900

    Add 562.cpp

--- UVa/100-1999/500-599/562.cpp
+++ UVa/100-1999/500-599/562.cpp
@@ -1,26 +1,43 @@
 // UVa 562 - Dividing coins
+// DP (Knapsack problem)
 #include <cstdio>
+#include <cstring>
 using namespace std;
 
-/*
-(id, remW)
-0 - ignore
-1 - take
-*/
-int val[105][100*500], V[105];
-int m;
+int DP[105][100*500+1] = {0,}, V[105];
+int m, sum, target;
 
-int knapsack(int id, int remVal){
-    val[id][0] = 0;
+int max(int a, int b){
+    return a > b ? a : b;
+}
 
-    for(int i = m-1; i >= 0; i--){
-        for(int remW = 
+void knapsack(void){
+    for(int id = m-1; id >= 0; id--){
+        for(int v = sum/2; v >= 0; v--){
+            if(V[id] > v) DP[id][v] = DP[id+1][v];
+            else DP[id][v] = max(DP[id+1][v], V[id] + DP[id+1][v-V[id]]); // max(ignore, take)
+        }
     }
 }
 
 int main(void){
-    scanf("%d", &m);
+    int TC;
+    
+    scanf("%d", &TC);
 
-    for(int i = 0; i < m; i++) scanf("%d", &
+    while(TC--){
+        scanf("%d", &m);
+        memset(DP, 0, sizeof(DP));
+        sum = 0;
 
+        for(int i = 0; i < m; i++){
+            scanf("%d", &V[i]);
+            sum += V[i];
+        }
+        knapsack();
+        
+        printf("%d\n", sum - 2*DP[0][sum/2]);
+    }
+
+    return 0;
 }
