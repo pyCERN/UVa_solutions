342d1cf64467548918669425cc3c50870f88e629
JunGu Kang <gjk6626@gmail.com>
2021-12-31 21:24:14 +0900

    Update 1213.cpp

--- UVa/100-1999/1200-1299/1213.cpp
+++ UVa/100-1999/1200-1299/1213.cpp
@@ -1,4 +1,5 @@
-// UVa 1213 - 
+// UVa 1213 - Sum of Different Primes
+// DP (knapsack problem)
 #include <cstdio>
 #include <cstring>
 using namespace std;
@@ -26,19 +27,4 @@ int knapsack(int n, int k, int i){
     DP[n][k][i] = knapsack(n, k, i+1) + knapsack(n-primes[i], k-1, i+1);
 
     return DP[n][k][i];
-}
-
-int main(void){
-    memset(DP, -1, sizeof(DP));
-    nPrimes = 0;
-
-    for(int n = 2; n <= 1130; n++){
-        if(isPrime(n)) primes[++nPrimes] = n;
-    }
-
-    while(scanf("%d %d", &N, &K), N | K){
-        printf("%d\n", knapsack(N, K, 1));
-    }
-
-    return 0;
 }
