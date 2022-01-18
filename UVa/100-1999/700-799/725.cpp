72d9848227c53b5ddb774afd8fd845cba0943cdb
pycern <pycern@pycernui-MacBookAir.local>
2022-01-18 23:33:21 +0900

    Update 725.cpp

--- UVa/100-1999/700-799/725.cpp
+++ UVa/100-1999/700-799/725.cpp
@@ -1,22 +1,33 @@
-#include <iostream>
+// UVa 725 - Division
+// Complete search
+#include <cstdio>
+#include <bitset>
 using namespace std;
 
-// Complete Search 
-
 int main(void){
-	int N = 62;
+	int TC = 0, N;
+	bitset<10> used;
 
-	for(int fghij = 1234; fghij < 98765 / N; fghij++){
-		int abcde = fghij * N;
-		int tmp, diff = (fghij < 10000);
-		tmp = abcde;
-		while(tmp) { diff |= 1 << (tmp % 10); tmp /= 10; }
-		tmp = fghij;
-		while(tmp) { diff |= 1 << (tmp % 10); tmp /= 10; }
+	while(scanf("%d", &N), N != 0){
+		int num = 0;
+		if(TC > 0) printf("\n");
 
-		if(diff == (1 << 10) -1)
-			printf("%.5d, %.5d\n", abcde, fghij);
+		for(int fghij = 1234; fghij <= 98765/N; fghij++){
+			bool f = (fghij < 10000);
+			int abcde = fghij * N;
+			// digits check for abcde, fghij
+			used.set();
+			for(int tmp = fghij; tmp > 0; tmp /= 10) used.set(tmp%10, 0);
+			for(int tmp = abcde; tmp > 0; tmp /= 10) used.set(tmp%10, 0);
+			if(f) used.set(0, 0);
+			if(!used.any()){
+				printf("%0.5d / %0.5d = %d\n", abcde, fghij, N);
+				num++;
+			}
+		}
+		if(num == 0) printf("There are no solutions for %d.\n", N);
+		TC++;
 	}
+
 	return 0;
 }
-
