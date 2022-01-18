888e74346020f064ceb82e09abfef685e9fd9b93
pycern <pycern@pycernui-MacBookAir.local>
2022-01-18 23:49:29 +0900

    Update 441.cpp

--- UVa/100-1999/400-499/441.cpp
+++ UVa/100-1999/400-499/441.cpp
@@ -11,11 +11,11 @@ int main(void){
 		if(TC > 0) printf("\n");
 		for(int i = 0; i < N; i++) scanf("%d", &S[i]);
 
-		for(int i = 0; i < N; i++){
-			for(int j = i+1; j < N; j++){
-				for(int k = j+1; k < N; k++){
-					for(int l = k+1; l < N; l++){
-						for(int m = l+1; m < N; m++){
+		for(int i = 0; i < N-5; i++){
+			for(int j = i+1; j < N-4; j++){
+				for(int k = j+1; k < N-3; k++){
+					for(int l = k+1; l < N-2; l++){
+						for(int m = l+1; m < N-1; m++){
 							for(int n = m+1; n < N; n++) printf("%d %d %d %d %d %d\n", S[i], S[j], S[k], S[l], S[m], S[n]);
 						}
 					}
