3d075749635652a40c5d7485e024c55f9ad63cf7
pycern <pycern@pycernui-MacBookAir.local>
2022-01-18 19:51:43 +0900

    Update 599.cpp

--- UVa/100-1999/500-599/599.cpp
+++ UVa/100-1999/500-599/599.cpp
@@ -1,3 +1,5 @@
+// UVa 599 - The Forrest for the Trees
+// Graph structure
 #include <iostream>
 #include <cstdio>
 #include <string>
@@ -8,24 +10,24 @@ int main(void){
     int nVertices, nEdges, nAcorns, TC;
     string line;
     bitset<26> hasEdge;
-    FILE *fp = fopen("ans.txt", "w");
+
     scanf("%d", &TC);
     fflush(stdin); // prevent line[0] = '\n'
 
     while(TC--){
         nEdges = nAcorns = 0; hasEdge.reset();
-        while(getline(cin, line), line[0] == '('){ // input edges
+        while(cin >> line, line[0] == '('){ // input edges
             nEdges += 1;
             hasEdge.set(line[1]-65, 1);
             hasEdge.set(line[3]-65, 1);
         }
-        getline(cin, line); // input vertices
+        cin >> line; // input vertices
         nVertices = (line.size() + 1) / 2;
         for(int i = 0; i < line.size(); i += 2){
-            if(!hasEdge[line[i]-65]) nAcorns++;
+            if(hasEdge[line[i]-65] == 0) nAcorns++;
         }
-        fprintf(fp, "There are %d tree(s) and %d acorn(s).\n", nVertices-nEdges-nAcorns, nAcorns);
+        printf("There are %d tree(s) and %d acorn(s).\n", nVertices-nEdges-nAcorns, nAcorns);
     }
-    fclose(fp);
+
     return 0;
 }
