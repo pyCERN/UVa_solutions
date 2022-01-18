d3bc2c69af2e464c33ba68b3b339be027bcd5563
pycern <pycern@pycernui-MacBookAir.local>
2022-01-19 00:39:43 +0900

    Update 11565.cpp

--- UVa/10000-/11500-11599/11565.cpp
+++ UVa/10000-/11500-11599/11565.cpp
@@ -1,20 +1,29 @@
-#include <iostream>
-
-// Complete Search
+// UVa 11565 - Simple Equations
+// Complete search
+#include <cstdio>
 
 int main(void){
-	int A, B, C;
+	int N, A, B, C;
 
-	scanf("%d %d %d", &A, &B, &C);
-	for(int x = -22; x <= 22; x++){			// x = y = z
-		for(int y = -100; y < 100; y++){
-			for(int z = -100; z < 100; z++){
-				if(x != y && y != z && z != x){
-					if(x + y + z == A && x * y * z == B && x * x + y * y + z * z == C)
+	scanf("%d", &N);
+	while(N--){
+		scanf("%d %d %d", &A, &B, &C);
+		bool done = false;
+
+		for(int x = -100; x <= 100 && !done; x++){
+			for(int y = -100; y <= 100 && !done; y++){
+				for(int z = -100; z <= 100 && !done; z++){
+					if(!(x | y | z)) continue;
+					if(x == y || y == z || z == x) continue;
+					if(x + y + z == A && x * y * z == B && x*x + y*y + z*z == C){
 						printf("%d %d %d\n", x, y, z);
+						done = true;
+					}
 				}
 			}
 		}
+		if(!done) printf("No solution.\n");
 	}
+
 	return 0;
 }
