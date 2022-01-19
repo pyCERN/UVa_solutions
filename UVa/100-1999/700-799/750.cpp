887e42c6e0424033b1696158cc62815100e539b2
pycern <39230856+pyCERN@users.noreply.github.com>
2022-01-19 22:11:02 +0900

    Update 750.cpp

--- UVa/100-1999/700-799/750.cpp
+++ UVa/100-1999/700-799/750.cpp
@@ -1,43 +1,50 @@
+// UVa 750 - 8 Queens Chess Problem
+// Backtracking
 #include <iostream>
 #include <cstring>
-#include <cstdlib>
+#include <string>
+#define ABS(x) ((x) > 0 ? (x) : -(x))
+using namespace std;
 
-int row[8], a, b, lineCount;
+int row[10] = {0,}; // row[i] : position of queen at i-th row
+int R, C, nSol;
 
 bool check(int r, int c){
-	for(int prev = 0; prev < c; prev++)
-		if(row[prev] == r || abs(row[prev] - r) == abs(prev - c)) return false;
+	for(int prevRow = 1; prevRow < r; prevRow++){
+		if(c == row[prevRow] || r-prevRow == ABS(row[prevRow]-c)) return false;
+	}
 	return true;
 }
 
-void place(int c){
-	if(c == 8 && row[b] == a){
-		printf("%d ", lineCount++);
-		for(int i = 0; i < 8; i++){
-			printf("%d ", row[i] + 1);
-			if(i == 7) printf("\n");
-		}
+void backtrack(int r){
+	if(r == 9 && row[R] == C){
+		nSol++;
+		printf("%2d      ", nSol);
+		for(int rr = 1; rr <= 8; rr++) printf("%d ", row[rr]);
+		printf("\n");
+		return;
 	}
-	for(int r = 0; r < 8; r++){
-		if(check(r, c)){
-			row[c] = r;
-			place(c+1);
-		}
-	}
-}	
-		
-int main(void){
-	int n;
 
-	scanf("%d", &n);
-	while(n--){
-		scanf("%d %d", &a, &b); a--; b--;
-		memset(row, 0, sizeof row);
-		lineCount++;
-		printf("SOLN COLUMN\n");
-		printf("# 1 2 3 4 5 6 7 8\n\n");
-		place(0);
-		if(n) printf("\n");
+	for(int c = 1; c <= 8; c++){
+		if(check(r, c)){
+			row[r] = c;
+			backtrack(r+1);
+		}
 	}
+}
+
+int main(void){	
+	int TC;
+	string s;
+
+	scanf("%d", &TC); getline(cin, s); getline(cin, s);
+	while(TC--){
+		memset(row, 0, sizeof(row)); nSol = 0;
+		scanf("%d %d", &R, &C);
+		printf("SOLN       COLUMN\n");
+		printf(" #      1 2 3 4 5 6 7 8\n\n");
+		backtrack(1);
+	}
+
 	return 0;
 }
