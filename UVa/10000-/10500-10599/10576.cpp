8c6091e055d9d8440f47fd5d6422a39bdc34f52d
pycern <pycern@pycernui-MacBookAir.local>
2022-01-25 19:31:27 +0900

    Add 10576.cpp

--- UVa/10000-/10500-10599/10576.cpp
+++ UVa/10000-/10500-10599/10576.cpp
@@ -1,5 +1,7 @@
 // UVa 10576 - Y2K Accounting Bug
 #include <cstdio>
+#include <cstring>
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 /*
 0, 1, 2, 3, 4
@@ -10,7 +12,8 @@
 5, 6, 7, 8, 9
 6, 7, 8, 9, 10
 7, 8, 9, 10, 11
-
+0 1 2 3 4 5 6 7 8 9 10 11 12
+0 s s s s s s s s s  s  s  s
 2^12 total cases
 (s, s, ..., s)
 (s, s, ..., -d)
@@ -19,23 +22,40 @@
 */
 
 int s, d, surplus;
-int five[8];
+int accSum[7]; // save sum of [0..0], ..., [0..6]
+FILE *fp = fopen("ans.txt", "w");
 
-void backtrack(int i, int num){
+void backtrack(int i, int num){ // num : sum [1..i]
     if(i == 12){
+        surplus = MAX(surplus, num);
+        fprintf(fp, "%d %d\n", num, surplus);
         return;
     }
+    int five = 0;
+    
+    if(i < 7){
+        accSum[i] = num;
+    }
 
-    if(i >= 5){
-        
+    // check 5 five months, i = [4..11]
+    if(i >= 4){
+        if(i == 4) five = num;
+        else five = num-accSum[i-5]; // sum of [i-4..i]
+    }
+    if(five > 0) backtrack(i, num-d);
+    else{
+        backtrack(i+1, num+s);
+        backtrack(i+1, num-d);
     }
 }
 
 int main(void){
     scanf("%d %d", &s, &d);
+    memset(accSum, 0, sizeof(accSum));
     surplus = 0;
 
     backtrack(0, 0);
-
+    printf("%d\n", surplus);
+fclose(fp);
     return 0;
 }
