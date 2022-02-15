0284f6dbfd66e194a16ac3133e75bdd37c992d7e
pycern <pycern@pycernui-MacBookAir.local>
2022-02-16 06:03:22 +0900

    Update 12321.cpp

--- UVa/10000-/12000-12099/12321.cpp
+++ UVa/10000-/12000-12099/12321.cpp
@@ -1,38 +1,45 @@
 // UVa 12321 - Gas Stations
+// Interval covering problem (Greedy)
 #include <cstdio>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-bool leftmost(pair<int, int> a, pair<int, int> b){
-    return a.first < b.first;
-}
+typedef pair<int, int> ii;
 
-bool rightmost(pair<int, int> a, pair<int, int> b){
+bool compare(ii &a, ii &b){
+    if(a.first != b.first)
+        return a.first < b.first;
     return a.second > b.second;
-}
-
-bool isCovered(pair<int, int> a, pair<int, int> b){
-    if(b.first < a.first || b.second > a.second) return false;
-    return true;
 }
 
 int main(void){
     int L, G;
 
-    scanf("%d %d", &L, &G);
-    vector<pair<int, int>> station(G);
+    while(scanf("%d %d", &L, &G), L | G){
+        vector<ii> station(G);
+        int left = 0, right = 0, cnt = 0;
 
-    for(int i = 0; i < G; i++){
-        int x, r;
-        scanf("%d %d", &x, &r);
-        station[i] = make_pair(x-r, x+r);
+        for(int i = 0; i < G; i++){
+            int x, r;
+            scanf("%d %d", &x, &r);
+            station[i] = make_pair(max(x-r, 0), min(x+r, L));
+        }
+        
+        sort(station.begin(), station.end(), compare);
+        int i = 0;
+        while(left < L){
+            while(i < G && station[i].first <= left) // go as furthest as possible while left bound of interval <= min bound of current range
+                right = max(right, station[i++].second);
+            if(left == right) // right not changed -> interval cannot go further
+                break;
+            // select next interval as start
+            left = right;
+            cnt++;
+        }
+        if(right < L) printf("-1\n");
+        else printf("%d\n", G-cnt);
     }
-    
-    sort(station.begin(), station.end(), leftmost);
-    printf("\n");
-    sort(station.begin(), station.end(), rightmost);
-    for(int i = 0; i < G; i++) printf("%d %d\n", station[i].first, station[i].second);
 
     return 0;
 }
