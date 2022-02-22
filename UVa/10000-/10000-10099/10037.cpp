97e34b76f6fb3b40319e1cdfc644f915e15f904a
pycern <39230856+pyCERN@users.noreply.github.com>
2022-02-23 07:14:40 +0900

    Add 10037.cpp

---
+++ UVa/10000-/10000-10099/10037.cpp
@@ -0,0 +1,16 @@
+// UVa 10037 - Bridge
+// Greedy
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+    int N;
+
+    scanf("%d", &N);
+    vector<int> time(N);
+    for(int i = 0; i < N; i++) scanf("%d", &time[i]);
+
+    
+}
