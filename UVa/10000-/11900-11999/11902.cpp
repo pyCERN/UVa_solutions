f366f840225c6739dc4470486194e62a4c5fd701
pycern <39230856+pyCERN@users.noreply.github.com>
2022-02-27 04:19:24 +0900

    Add 11902.cpp

--- UVa/10000-/11900-11999/11902.cpp
+++ UVa/10000-/11900-11999/11902.cpp
@@ -1,80 +1,40 @@
-#include <iostream>
-#include <algorithm>
+// UVa 11902 - Dominator
+// DFS
+#include <cstdio>
+#include <cstring>
 #include <vector>
-#include <utility>
 using namespace std;
 
-// DFS
-
-typedef pair<int, int> ii;
 typedef vector<int> vi;
+typedef vector<vi> vvi;
 
-enum { UNVISITED = -1, VISITED = 1 };
+enum {UNVISITED = 0, VISITED};
+int adjList[101][101], dom[101][101];
+int visited[101];
+int N;
 
-vector<vi> AdjList;
-vi dfs_num;
-int domer, domee;
-bool first;
+// whether the path 0 -> Y contains X
+void dfs(){
 
-void print(int N){
-	printf("+");
-	for(int i = 0; i < 2*N-1; i++) printf("-");
-	printf("+\n");
 }
-
-void initialize(int N){
-	for(int i = 0; i < N; i++){
-		vi v;
-		for(int j = 0; j < N; j++){
-			int x;
-			scanf("%d", &x);
-			if(x) v.push_back(j);
-		}
-		AdjList.push_back(v);
-	}
-}
-
-void dfs(int u){
-	if(!first && u == domer) return;
-	dfs_num[u] = VISITED;
-	for(int i = 0; i < AdjList[u].size(); i++){
-		int x = AdjList[u][i]; if(dfs_num[x] == UNVISITED) dfs(x);
-	}
-}
-
-bool canReach(int target){
-	first = true;
-	dfs(0);
-	if(dfs_num[target] == UNVISITED) return false;
-	return true;
-}	
 
 int main(void){
-	int TC, N, c = 1;
+FILE *fp = fopen("ans.txt", "w");
+	scanf("%d", &N);
+	memset(adjList, 0, sizeof(adjList)); memset(dom, 0, sizeof(dom));
 
-	scanf("%d", &TC);
-	while(TC--){
-		scanf("%d", &N);
-		AdjList.clear();
-		initialize(N);
-
-		printf("Case %d:\n", c++);
-		for(int i = 0; i < N; i++){
-			domer = i;
-			print(N); printf("|");
-			for(int j = 0; j < N; j++){
-				dfs_num.assign(N, UNVISITED);
-				if(!canReach(j)) { printf("N|"); continue; }
-				first = false;
-				dfs_num.assign(N, UNVISITED);
-				domee = j;
-				dfs(0); 
-				if(dfs_num[j] == VISITED) printf("N|");
-				else printf("Y|");
-			}
-			printf("\n");
-		}
-		print(N);
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++) scanf("%d", &adjList[i][j]);
 	}
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++)
+		{
+			memset(visited, UNVISITED, sizeof(visited));
+			dfs(i, j);
+			fprintf(fp, "%d ", dom[i][j]);
+		}
+		fprintf(fp, "\n");
+	}
+fclose(fp);
 	return 0;
 }
